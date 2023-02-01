4d6b4b95b1e907c58c6b96f5dfc67a919abfe430
3888 <krrg3888@gmail.com>
2023-02-01 12:09:17 +0200

    added xylophone

--- lib/Playground/DevBootcamp2020/Xylophone/XylophoneApp.dart
+++ lib/Playground/DevBootcamp2020/Xylophone/XylophoneApp.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 
 final player = AudioPlayer();
 
-Future<void> setSource(Source source) async {
+Future<void> playSoundByNumber(int soundNumber) async {
   await player.stop();
-  await player.play(source);
+  await player.play(AssetSource('note$soundNumber'));
 }
 
 ButtonStyle raisedButtonStyle(Color background) => ElevatedButton.styleFrom(
       foregroundColor: Colors.grey[300],
       backgroundColor: background,
-      minimumSize: Size(88, 36),
-      padding: EdgeInsets.symmetric(horizontal: 16),
+      minimumSize: const Size(88, 36),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(2)),
       ),
@@ -22,17 +22,57 @@ xylophoneApp() => MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey[100],
         appBar: AppBar(
-          title: Text('Xylophone'),
+          title: const Text('Xylophone'),
           backgroundColor: Colors.red[100],
         ),
         body: SafeArea(
           child: Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  setSource(AssetSource('note1.wav'));
-                },
-                child: null,
-                style: raisedButtonStyle(Colors.red)),
+            child: Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      playSoundByNumber(1);
+                    },
+                    child: null,
+                    style: raisedButtonStyle(Colors.red)),
+                ElevatedButton(
+                    onPressed: () {
+                      playSoundByNumber(2);
+                    },
+                    child: null,
+                    style: raisedButtonStyle(Colors.orange)),
+                ElevatedButton(
+                    onPressed: () {
+                      playSoundByNumber(3);
+                    },
+                    child: null,
+                    style: raisedButtonStyle(Colors.yellow)),
+                ElevatedButton(
+                    onPressed: () {
+                      playSoundByNumber(4);
+                    },
+                    child: null,
+                    style: raisedButtonStyle(Colors.green)),
+                ElevatedButton(
+                    onPressed: () {
+                      playSoundByNumber(5);
+                    },
+                    child: null,
+                    style: raisedButtonStyle(Colors.blue)),
+                ElevatedButton(
+                    onPressed: () {
+                      playSoundByNumber(6);
+                    },
+                    child: null,
+                    style: raisedButtonStyle(Colors.indigo)),
+                ElevatedButton(
+                    onPressed: () {
+                      playSoundByNumber(7);
+                    },
+                    child: null,
+                    style: raisedButtonStyle(Colors.purple)),
+              ],
+            ),
           ),
         ),
       ),
